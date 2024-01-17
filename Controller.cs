a078d3371ae7c774453b39ab6c1242498fcd09ac
Alexandra Bunch <121991056+oldfashionedjelly@users.noreply.github.com>
2024-01-16 21:29:28 -0500

    Add files via upload

---
+++ Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Axle
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor;
+    public bool steering;
+}
+
+public class Controller : MonoBehaviour
+{
+    public List<Axle> axles;
+    public float maxMotorTorque;
+    public float maxSteeringAngle;
+
+    public void FixedUpdate()
+    {
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (Axle axle in axles)
+        {
+            if (axle.motor)
+            {
+                axle.leftWheel.motorTorque = motor;
+                axle.rightWheel.motorTorque = motor;
+            }
+            if (axle.steering)
+            {
+                axle.leftWheel.steerAngle = steering;
+                axle.rightWheel.steerAngle = steering;
+            }
+        }
+    }
+}
---
+++ JumpScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class JumpScript : MonoBehaviour
+{
+    public bool isGrounded = false;
+    public float jumpForce = 1500.0f;
+    public Vector3 jumpValue = new Vector3(0.0f, 2.0f, 0.0f);
+    Rigidbody rigidBody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rigidBody.AddForce(jumpValue * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
